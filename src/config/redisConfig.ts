bca7ff80dce8493941d01453612660a1ddf76b44
Hendry Widyanto <hendrywidyanto97@gmail.com>
2025-05-29 20:04:33 +0700

    Self-host redis

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -4,19 +4,11 @@ const redisConfig = {
   host: process.env.REDIS_HOST!,
   port: Number(process.env.REDIS_PORT),
   password: process.env.REDIS_PASSWORD!,
-  tls: {
-    rejectUnauthorized: false,
-  },
 };
 
 export const redisAdapterConfig: RedisClientOptions = {
-  url: `rediss://:${process.env.REDIS_PASSWORD!}@${process.env
+  url: `redis://default:${process.env.REDIS_PASSWORD!}@${process.env
     .REDIS_HOST!}:${process.env.REDIS_PORT!}/0`,
-  socket: {
-    tls: true,
-    host: process.env.REDIS_HOST!,
-    // ca: fs.readFileSync("./trip-nus-ca.crt"),
-  },
 };
 
-export default redisConfig
+export default redisConfig;
